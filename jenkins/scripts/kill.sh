f7968962fa95bd5e6ac9334f20f77c38ae9e3171
sensre <32806443+sensre@users.noreply.github.com>
2020-04-04 09:44:43 -0400

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -2,6 +2,5 @@
 
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when either'
-echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 set -x
 kill $(cat .pidfile)
